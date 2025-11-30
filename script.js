d9236d9af3bb1c34c5ae28d13e0e0a12f2032668
Kauan Bento Cerqueira <135340242+KauanBento@users.noreply.github.com>
2025-11-30 19:50:25 -0300

    Imagens adicionadas e mais faq, tambem resolvido bug do faq

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        entry.target.classList.add("visible");
+        entry.target.classList.add("visible"); 
         observer.unobserve(entry.target);
       }
     });
